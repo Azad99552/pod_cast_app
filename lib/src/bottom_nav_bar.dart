8b1e26319cc521e21de04a168e09a5c474bc0ab4
Azad99552 <54085555+Azad99552@users.noreply.github.com>
2022-11-12 07:26:29 +0530

    first commit

---
+++ lib/src/bottom_nav_bar.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:pod_cast_app/src/screens/favorite_screen.dart';
+import 'package:pod_cast_app/src/screens/home_screen.dart';
+import 'package:pod_cast_app/src/screens/trending_screen.dart';
+import 'package:pod_cast_app/src/user_menu.dart';
+
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  int _selectedIndex = 0;
+
+  ///List of Screens
+  List _pages = [
+    HomeScreen(),
+    FavoriteScreen(),
+    TrendingScreen(),
+    UserMenu(),
+  ];
+
+  ///List of Icons
+  List<IconData> _icons = [
+    Icons.home,
+    Icons.favorite_border,
+    Icons.blur_on,
+    Icons.person,
+  ];
+
+  ///Building Bottom Navigation Bar Item
+  BottomNavigationBarItem _buildIcons(int index) {
+    return BottomNavigationBarItem(
+      // ignore: deprecated_member_use
+      title: Text(''),
+      icon: Icon(
+        _icons[index],
+        color: _selectedIndex == index ? Colors.pink : Colors.grey,
+        size: 35,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: UserMenu(),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(25.0),
+          topRight: Radius.circular(25.0),
+        ),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _selectedIndex,
+          onTap: (index) {
+            index == 3
+                ? _scaffoldKey.currentState.openDrawer()
+                : setState(() {
+                    _selectedIndex = index;
+                  });
+          },
+          items: _icons
+              .asMap()
+              .entries
+              .map(
+                (mapEntry) => _buildIcons(mapEntry.key),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
